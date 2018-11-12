0885b6c80c6a37823d526c7d6d62dec9b26cf240
santillano1996 <luissantillano1996@gmail.com>
2018-11-12 00:24:33 -0700

    no message

---
+++ Eva2_25_polimorfismo/src/quique/Avion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quique;
+
+/**
+ *
+ * @author toshiba
+ */
+public class Avion extends Vehiculo{
+    public void movimiento(){
+        System.out.print("el avion vuela");
+    }
+    
+}
